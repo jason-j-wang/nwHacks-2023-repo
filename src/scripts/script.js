698a1d3029f075a7eae7da16c6c9978794306ccb
Stevan Zhuang <szhuang1@students.cs.ubc.ca>
2023-01-21 14:04:48 -0800

    changed api call

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,19 +2,14 @@ async function fetchData() {
 	const options = {
 		method: 'GET',
 		headers: {
-			'X-RapidAPI-Key': '',
+			'X-RapidAPI-Key': '58a419ef3amshf0356c674257641p181b37jsnd43b8bf5e5e5',
 			'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
 		}
 	};
-
-	const res = await fetch('https://sportscore1.p.rapidapi.com/events/live?page=1', options)
+	
+	fetch('https://sportscore1.p.rapidapi.com/events/live?page=1', options)
 		.then(response => response.json())
 		.then(response => console.log(response))
 		.catch(err => console.error(err));
-
-	const record = await res.json();
-	document.getElementById("team1").innerHTML=record.data[0].team1;
-	document.getElementById("team2").innerHTML=record.data[0].team2;
-
 }
 fetchData();
